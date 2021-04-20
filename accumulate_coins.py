0b0846b5dad2abf84f843b302c391438656a4f7f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-20 08:49:04 -0300

    add logs

--- accumulate_coins.py
+++ accumulate_coins.py
@@ -1,11 +1,13 @@
+import os
 from binance.client import Client
 from binance.exceptions import BinanceAPIException
-import os
+from logger.app_logger import AppLogger
 
 
 class AccumulateCoins:
     def __init__(self):
         self.client = Client(os.getenv("BINANCE_API"), os.getenv("BINANCE_SECRET"))
+        self.logger = AppLogger().get()
 
     def get_usdt_futures_balance(self):
         details = self.client.futures_account_balance()
@@ -21,14 +23,14 @@ class AccumulateCoins:
                 return i["free"]
 
     def print_balance(self):
-        print("Spot Balance in USDT is: ", self.get_usdt_spot_balance())
-        print("Futures Balance in USDT is: ", self.get_usdt_futures_balance())
+        self.logger.info("Spot Balance in USDT is: %s", self.get_usdt_spot_balance())
+        self.logger.info("Futures Balance in USDT is: %s", self.get_usdt_futures_balance())
 
     def get_value(self):
         value = input("Enter the value to transfer from Futures to spot wallet: ")
 
         if value > self.get_usdt_futures_balance():
-            print("Insufficient balance, Enter the values again.")
+            self.logger.warning("Insufficient balance, Enter the values again.")
             self.get_value()
         else:
             try:
@@ -36,8 +38,8 @@ class AccumulateCoins:
                     asset="USDT", amount=value, type=2, timestamp=2348273489823
                 )
                 print("Balance transfer succesful !!!")
-            except BinanceAPIException as error:
-                pass
+            except BinanceAPIException as e:
+                self.logger.error('There was an API error: %s', e)
 
     def run(self):
         self.print_balance()
