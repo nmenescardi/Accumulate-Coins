5291725241ff2965bedc421270b56a38a56dc384
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-20 09:10:36 -0300

    fix lint issues

--- accumulate_coins.py
+++ accumulate_coins.py
@@ -1,54 +1,60 @@
+"""Script for transfering funds from the Binance futures wallets into spot and accumulate coins."""
 import os
 
-from binance.client import Client
-from binance.exceptions import BinanceAPIException
+from binance.client import Client  # type: ignore
+from binance.exceptions import BinanceAPIException  # type: ignore
 
 from logger.app_logger import AppLogger
 
 
 class AccumulateCoins:
+    """ Main class to control the process """
+
     def __init__(self):
         self.client = Client(os.getenv("BINANCE_API"), os.getenv("BINANCE_SECRET"))
         self.logger = AppLogger().get()
 
-    def get_usdt_futures_balance(self):
+    def _get_usdt_futures_balance(self):
         details = self.client.futures_account_balance()
         for i in details:
             if i["asset"] == "USDT":
                 return i["balance"]
+        return 0
 
-    def get_usdt_spot_balance(self):
+    def _get_usdt_spot_balance(self):
         details = self.client.get_account()
         balance_details = details["balances"]
         for i in balance_details:
             if i["asset"] == "USDT":
                 return i["free"]
+        return 0
 
-    def print_balance(self):
-        self.logger.info("Spot Balance in USDT is: %s", self.get_usdt_spot_balance())
+    def _print_balance(self):
+        self.logger.info("Spot Balance in USDT is: %s", self._get_usdt_spot_balance())
         self.logger.info(
-            "Futures Balance in USDT is: %s", self.get_usdt_futures_balance()
+            "Futures Balance in USDT is: %s", self._get_usdt_futures_balance()
         )
 
-    def get_value(self):
+    def _get_value(self):
         value = input("Enter the value to transfer from Futures to spot wallet: ")
 
-        if value > self.get_usdt_futures_balance():
+        if value > self._get_usdt_futures_balance():
             self.logger.warning("Insufficient balance, Enter the values again.")
-            self.get_value()
+            self._get_value()
         else:
             try:
                 self.client.futures_account_transfer(
                     asset="USDT", amount=value, type=2, timestamp=2348273489823
                 )
                 print("Balance transfer succesful !!!")
-            except BinanceAPIException as e:
-                self.logger.error("There was an API error: %s", e)
+            except BinanceAPIException as error:
+                self.logger.error("There was an API error: %s", error)
 
     def run(self):
-        self.print_balance()
-        self.get_value()
-        self.print_balance()
+        """Main entry point"""
+        self._print_balance()
+        self._get_value()
+        self._print_balance()
 
 
 if __name__ == "__main__":
