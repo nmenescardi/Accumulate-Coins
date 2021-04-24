73b61de064dfc8e2fa86c51dcee64608d835a2db
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-04-23 22:58:04 -0300

    modify entry point to include strategies manager

--- accumulate_coins.py
+++ accumulate_coins.py
@@ -2,6 +2,7 @@
 
 from wallets.futures import Futures as FuturesWallet
 from wallets.spot import Spot as SpotWallet
+from strategies.manager import Manager as StrategiesManager
 
 
 class AccumulateCoins:
@@ -10,6 +11,7 @@ class AccumulateCoins:
     def __init__(self):
         self.futures_wallet = FuturesWallet()
         self.spot_wallet = SpotWallet()
+        self.strategies_manager = StrategiesManager()
 
     def _print_balance(self):
         self.futures_wallet.print_balance()
@@ -18,7 +20,7 @@ class AccumulateCoins:
     def run(self):
         """Main entry point"""
         self._print_balance()
-        self.futures_wallet.transfer_to_spot()
+        self.strategies_manager.run()
         self._print_balance()
 
 
