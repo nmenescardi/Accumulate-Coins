26ba7493441ec81b0eaf4b8b513d25bfc02da7cc
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-19 08:03:30 -0300

    add async client

--- client/client.py
+++ client/client.py
@@ -3,6 +3,7 @@
 import os
 
 from binance.client import Client as BinanceClient  # type: ignore
+from binance.client import AsyncClient as AsyncBinanceClient  # type: ignore
 
 from helpers.singleton import singleton
 
@@ -15,7 +16,14 @@ class Client:
         self.client = BinanceClient(
             os.getenv("BINANCE_API"), os.getenv("BINANCE_SECRET")
         )
+        self.asyncClient = AsyncBinanceClient(
+            os.getenv("BINANCE_API"), os.getenv("BINANCE_SECRET")
+        )
 
     def get(self):
         """Get client instance."""
         return self.client
+
+    def getAsync(self):
+        """Get async client instance."""
+        return self.asyncClient
