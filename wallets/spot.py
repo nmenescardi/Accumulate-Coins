4ef1c35f4196ece3976f31b95f1bcd347beabda7
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-05-20 06:39:52 -0300

    fetch min notional

--- wallets/spot.py
+++ wallets/spot.py
@@ -36,7 +36,7 @@ class Spot(AbstractWallet):
 
         return was_placed
 
-    def get_quantity(self, price, amount, symbol):
+    def get_quantity(self, price, original_amount, symbol):
         """ calculates quantity to buy based on last price and the given amount """
 
         self.logger.debug('getting quantity for %s', symbol)
@@ -45,28 +45,38 @@ class Spot(AbstractWallet):
         base_fee = self.client.get_trade_fee(symbol=symbol)[0]['takerCommission'] or 0.001
         fee = (100 - float(base_fee)) / 100
         
+        min_notional = self._get_min_notional(symbol=symbol)
+        amount = original_amount if original_amount >= min_notional else min_notional * 1.01 
+
         base_quantity = (amount / price) * fee
         precision = int(round(-math.log(step_size, 10), 0))
         quantity = float(round(base_quantity, precision))
-        
+
         self.logger.debug(
-            '%s Final Qty: %s.. Precision: %s.. Base Qty: %s.. Base Fee: %s.. Fee: %s.. ',
+            '%s Final Qty: %s.. Precision: %s.. Base Qty: %s.. Base Fee: %s.. Fee: %s.. Final Amount: %s.. Original Amount: %s.. MIN Notional: %s',
             symbol,
             quantity,
             precision,
             base_quantity,
             base_fee,
-            fee
+            fee,
+            amount,
+            original_amount,
+            min_notional
         )
         return quantity
 
+    def _get_min_notional(self, symbol):
+        min_notional = self._get_from_symbol_info_filters(symbol, 'MIN_NOTIONAL', 'minNotional') or 11
+        return float(min_notional)
+
     def _get_step_size(self, symbol):
-        step_size = self._get_from_symbol_info_filters(symbol, 'LOT_SIZE') or 0.0001
+        step_size = self._get_from_symbol_info_filters(symbol, 'LOT_SIZE', 'stepSize') or 0.0001
         return float(step_size)
 
-    def _get_from_symbol_info_filters(self, symbol, filter_type):
+    def _get_from_symbol_info_filters(self, symbol, filter_type, prop):
         sym_info = self.client.get_symbol_info(symbol)
         filters = sym_info['filters']
         for f in filters:
             if f['filterType'] == filter_type:
-                return f['stepSize']
+                return f[prop]
