c895587fe9274af275f1a9e5bfabe178710fd6cc
Nicolas Menescardi <nicolasmenescardi@gmail.com>
2024-02-11 14:44:55 +0000

    update dependencies and strategy

--- strategies/config.py
+++ strategies/config.py
@@ -11,25 +11,25 @@ config = {
             "limit": 26,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
     "BTCUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 30,
             "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 750 * factor,
-            "max_period": 35,
+            "amount": 500 * factor,
+            "max_period": 31,
         },
     },
     "ETHUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 29,
             "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 500 * factor,
-            "max_period": 35,
+            "amount": 300 * factor,
+            "max_period": 31,
         },
     },
     "ADAUSDT": {
@@ -38,115 +38,124 @@ config = {
             "limit": 26,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
-    "ATOMUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 26,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 25 * factor,
-            "max_period": 35,
-        },
-    },
+    # "ATOMUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 25 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
     "DOTUSDT": {
         BelowRSI: {
             "period": 30,
             "limit": 26,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
-    "FTMUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 26,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 25 * factor,
-            "max_period": 35,
-        },
-    },
+    # "FTMUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 25 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
     "LINKUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 31,
             "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 25 * factor,
-            "max_period": 35,
+            "amount": 50 * factor,
+            "max_period": 31,
         },
     },
     "MATICUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 29,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
-    "QTUMUSDT": {
+    "SOLUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 5 * factor,
-            "max_period": 35,
-        },
-    },
-    "UNIUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 24,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 5 * factor,
-            "max_period": 35,
-        },
-    },
-    "VETUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 26,
+            "limit": 29,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
+    # "QTUMUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 5 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
+    # "UNIUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 5 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
+    # "VETUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 25 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
     "XRPUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 29,
             "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 25 * factor,
-            "max_period": 35,
+            "amount": 50 * factor,
+            "max_period": 31,
         },
     },
-    "ZRXUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 26,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 25 * factor,
-            "max_period": 35,
-        },
-    },
+    # "ZRXUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 25 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
     "TRXUSDT": {
         BelowRSI: {
             "period": 30,
-            "limit": 26,
+            "limit": 31,
             "timeframe": KLINE_INTERVAL_1HOUR,
             "amount": 25 * factor,
-            "max_period": 35,
+            "max_period": 31,
         },
     },
-    "RENUSDT": {
-        BelowRSI: {
-            "period": 30,
-            "limit": 26,
-            "timeframe": KLINE_INTERVAL_1HOUR,
-            "amount": 10 * factor,
-            "max_period": 35,
-        },
-    },
+    # "RENUSDT": {
+    #     BelowRSI: {
+    #         "period": 30,
+    #         "limit": 24,
+    #         "timeframe": KLINE_INTERVAL_1HOUR,
+    #         "amount": 10 * factor,
+    #         "max_period": 31,
+    #     },
+    # },
 }
